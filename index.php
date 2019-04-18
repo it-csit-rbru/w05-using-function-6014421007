a1d18a5ea1c9cbe2b9349fa71ecd667b98286954
6014421007 <6014421007@rbru.ac.th>
2019-04-18 23:52:46 +0700

    6014421007-w05

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $num = [10, 20, 30, 40, 50];
+
+        echo "SUM : ".sum($num)."<br>";
+        echo "MAX : ".max($num)."<br>";
+        echo "MIN : ".min($num)."<br>";
+        echo "COS : ".cos($num."<br>";
+        echo "TAN : ".tan($num)."<br>";
+        ?>
